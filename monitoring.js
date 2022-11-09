20917422000befc33d5dc39c78a569a9648b1b2e
drScripts <nathanael.vd@gmail.com>
2022-11-10 00:52:05 +0700

    chore: change relation field name

--- monitoring.js
+++ monitoring.js
@@ -1,6 +1,6 @@
 const { mongoClient } = require("./config")
 
-const database = mongoClient.db('replication');
+const database = mongoClient.db('636b7a77cbff4c962e39e9f1');
 const monitoring = database.collection('monitorings');
 
 const create = async (metrics) => {
