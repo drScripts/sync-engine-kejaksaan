75bb3131021ef4baf8d9d434d0f45fc7fff043b7
rizky tanjung <kimsohyuncans@gmail.com>
2022-11-09 07:19:01 +0700

    handle relation

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const cron = require('node-cron');
-const { getAuditLogs,updateFlagLog } = require('./pg')
+const { getAuditLogs, updateFlagLog } = require('./pg')
 const { insert, update, _delete } = require('./syncer')
 
 cron.schedule("*/10 * * * * *", async function () {
     try {
         const rows = await getAuditLogs()
         rows.map((row) => {
-            if(!row.is_synced) {
+            if (!row.is_synced) {
                 switch (row.action) {
                     case "I":
                         const insertedData = JSON.parse(row.new_data)
-                        insert(insertedData).then(() => {
+                        insert(row.table_name, insertedData).then(() => {
                             updateFlagLog(row.id)
                         })
                         break;
                     case "D":
                         const deletedData = JSON.parse(row.original_data)
-                        _delete(deletedData.id).then(() => {
+                        _delete(row.table_name, deletedData.id).then(() => {
                             updateFlagLog(row.id)
                         })
                         break;
                     case "U":
                         const updatedData = JSON.parse(row.new_data)
-                        update(updatedData.id, updatedData).then(() => {
+                        update(row.table_name, updatedData.id, updatedData).then(() => {
                             updateFlagLog(row.id)
                         }).catch(console.log)
                         break;
--- syncer.js
+++ syncer.js
@@ -2,40 +2,79 @@ const { mongoClient } = require("./config")
 const { ObjectId } = require("mongodb")
 
 const database = mongoClient.db('replication');
-const todo = database.collection('todo');
 const idMap = database.collection("idMaps")
 
-const getTargetId = async (sourceId) => {
-    const res = await idMap.findOne({ sourceId })
+const relationFields = [
+    "satKerId",
+    "locationId",
+    "caseTypeId",
+    "caseStageId",
+]
+
+const relationFieldWithForeignKey = {
+    "satKerId": "satkers",
+    "locationId": "locations",
+    "caseTypeId": "caseTypes",
+    "caseStageId": "caseStages",
+}
+
+const handleRelationField = async (data) => {
+    const parsedData = await Promise.all(Object.entries(data).map(async([field,value]) => {
+        if(relationFields.includes(field)) {
+            return { [field]: await getTargetId(relationFieldWithForeignKey[field], value) }
+        } else {
+            return { [field]: value }
+        }
+    }))
+
+    return Object.assign({}, ...parsedData)
+}
+
+const getTargetId = async (tableName, sourceId) => {
+    const res = await idMap.findOne({ sourceId, tableName })
+    if (!res) {
+        console.log("failed translate id source to target", {
+            tableName,
+            sourceId,
+            res
+        })
+    }
     return res.targetId
 }
 
-const createIdMap = (sourceId, targetId) => {
-    return idMap.insertOne({sourceId, targetId })
+const createIdMap = (tableName, sourceId, targetId) => {
+    return idMap.insertOne({ sourceId, targetId, tableName })
 }
 
-const insert = async (data) => {
-    const newData = {...data}
-    if(newData.id) {
+const insert = async (tableName, data) => {
+    let newData = { ...data }
+    if(tableName === "cases") {
+        newData = await handleRelationField(data)
+    }
+
+    if (newData.id) {
         newData._id = new ObjectId(newData.id)
         delete newData.id
     }
-    const res = await todo.insertOne(newData)
-    await createIdMap(data.id, res.insertedId)
+    const res = await database.collection(tableName).insertOne(newData)
+    await createIdMap(tableName, data.id, res.insertedId)
 }
 
-const update = async (id, data) => {
-    const targetId = await getTargetId(id)
-    const newData = {...data}
+const update = async (tableName, id, data) => {
+    let newData = { ...data }
+    if(tableName === "cases") {
+        const data = await handleRelationField(data)
+    }
     delete newData.id
-    await todo.updateOne({
+    const targetId = await getTargetId(tableName,id)
+    await database.collection(tableName).updateOne({
         _id: targetId,
-    }, { $set: newData})
+    }, { $set: newData })
 }
 
-const _delete = async (id) => {
-    const targetId = await getTargetId(id)
-    return todo.deleteOne({
+const _delete = async (tableName, id) => {
+    const targetId = await getTargetId(tableName,id)
+    return database.collection(tableName).deleteOne({
         _id: targetId,
     })
 }
